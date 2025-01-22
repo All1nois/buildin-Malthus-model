b1518b38607441c1669864463d824e013e4c041f
alina@gmail.com <kaifovayapochta@gmail.com>
2025-01-22 16:11:37 +0300

    Добавьте файлы проекта.

---
+++ PracticOne/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace PracticOne
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.endYear = new System.Windows.Forms.TextBox();
+            this.startYear = new System.Windows.Forms.TextBox();
+            this.people = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.plotView1 = new OxyPlot.WindowsForms.PlotView();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Численность:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Начальный год:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 171);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Конечный год:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.endYear);
+            this.groupBox1.Controls.Add(this.startYear);
+            this.groupBox1.Controls.Add(this.people);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.StartBtn);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(285, 381);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Настройки";
+            // 
+            // endYear
+            // 
+            this.endYear.Location = new System.Drawing.Point(26, 191);
+            this.endYear.Name = "endYear";
+            this.endYear.Size = new System.Drawing.Size(100, 22);
+            this.endYear.TabIndex = 17;
+            // 
+            // startYear
+            // 
+            this.startYear.Location = new System.Drawing.Point(26, 135);
+            this.startYear.Name = "startYear";
+            this.startYear.Size = new System.Drawing.Size(100, 22);
+            this.startYear.TabIndex = 16;
+            // 
+            // people
+            // 
+            this.people.Location = new System.Drawing.Point(24, 81);
+            this.people.Name = "people";
+            this.people.Size = new System.Drawing.Size(100, 22);
+            this.people.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(23, 260);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 16);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "?";
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(6, 335);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(273, 40);
+            this.StartBtn.TabIndex = 11;
+            this.StartBtn.Text = "Start";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // plotView1
+            // 
+            this.plotView1.Location = new System.Drawing.Point(505, 139);
+            this.plotView1.Name = "plotView1";
+            this.plotView1.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotView1.Size = new System.Drawing.Size(75, 23);
+            this.plotView1.TabIndex = 7;
+            this.plotView1.Text = "plotView1";
+            this.plotView1.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotView1.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotView1.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(320, 31);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(459, 392);
+            this.chart1.TabIndex = 8;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 234);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 16);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Население будет:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.plotView1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox endYear;
+        private System.Windows.Forms.TextBox startYear;
+        private System.Windows.Forms.TextBox people;
+        private OxyPlot.WindowsForms.PlotView plotView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PracticOne/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace PracticOne
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+           
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            try {
+                //Настраиваемые поля
+                double a = Convert.ToDouble(startYear.Text);
+                double b = Convert.ToDouble(endYear.Text);
+                double size = Convert.ToDouble(people.Text);
+                //Рождаемость
+                double BirthPeople = 0.028;
+                //Смертность
+                double DeathPeople = 0.008;
+                //Переменная для вывода результата
+                double outcom = 0;
+
+
+
+                string s;
+
+                //Область графика
+                chart1.ChartAreas.Add(new ChartArea("Метод"));
+
+
+                Series series = new Series("Численность");
+                series.ChartType = SeriesChartType.Line;
+
+
+
+                for (double i = a; a <= b; a++)
+                {
+                    outcom = size + (BirthPeople - DeathPeople) * size;
+                    size = outcom;
+
+                    label6.Text = Math.Round(size).ToString();
+
+
+                    for (double x = -Math.PI; x <= Math.PI; x += Math.PI / 10.0)
+                    {
+                        series.Points.Add(outcom, Math.Sin(size));
+
+
+                    }
+
+
+                }
+
+
+                chart1.Series.Add(series);
+
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка данных!");
+            }
+
+
+        }
+    }
+}
